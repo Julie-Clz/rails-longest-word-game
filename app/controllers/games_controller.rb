127c0f51f1335b874e94f01b2d7d7886293a05f3
julie collazos <juliecollazos@hotmail.com>
2020-08-10 17:40:23 +0200

    done w/o optional

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,9 +6,6 @@ class GamesController < ApplicationController
     @grid = 10.times.map { @letters.sample }
   end
 
-  def include?(guess, grid)
-    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
-  end
 
   def english_word?(word)
     response = open("https://wagon-dictionary.herokuapp.com/#{word}")
@@ -17,16 +14,16 @@ class GamesController < ApplicationController
   end  
 
   def score
-    @word = params[:input]
+    @word = params[:input].upcase
     grid = params[:grid].split(" ")
-    if include?(@word.upcase, grid)
+    if  @word.chars.all? { |letter| grid.include?(letter) } == true
       if english_word?(@word)
-        @score = "Congratulations! #{@word.upcase} is a valid English word!"
+        @score = "Congratulations! #{@word} is a valid English word!"
       else
-        @score = "Sorry but #{@word.upcase} does not seem to be a valid English word..."
+        @score = "Sorry but #{@word} does not seem to be a valid English word..."
       end
     else
-      @score = "Sorry but #{@word.upcase} can't be built out of #{grid.join(' ')}"
+      @score = "Sorry but #{@word} can't be built out of #{grid.join(' ')}"
     end
   end
 end
